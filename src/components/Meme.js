0ef19748c76b3008d2cf1b9a425da0ce091bb0cd
Isaac Vieira <isaacvf.dev@gmail.com>
2024-02-07 14:44:33 -0300

    removed the word img from alt

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -65,7 +65,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" alt="random meme image"/>
+                <img src={meme.randomImage} className="meme--image" alt="random meme"/>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
